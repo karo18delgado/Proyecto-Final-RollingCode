df67a522284b3e0324e5e5099d09082e2bb2ee2a
Agustin Gomez Urrutia <agustin.gu20@gmail.com>
2021-05-09 22:10:01 -0300

    formulario de registro y correcciones responsive navbar

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "./components/FontawesomeIcons"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import NavbarR from "./components/NavbarR";
+import RegisterForm from "./components/RegisterForm"
 
 function App() {
   return (
     <Router>
       <NavbarR />
+      <Switch>
+        <Route path="/register">
+          <RegisterForm />
+        </Route>
+      </Switch>
     </Router>
   );
 }
