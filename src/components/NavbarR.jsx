b28a3c9c3f7f93605f5482400dfdf0429b88cfd7
Agustin Gomez Urrutia <agustin.gu20@gmail.com>
2021-05-11 19:03:35 -0300

    centrado de subtitulo formulario de registro

--- src/components/NavbarR.jsx
+++ src/components/NavbarR.jsx
@@ -17,7 +17,7 @@ const title = <FontAwesomeIcon icon="shopping-bag"></FontAwesomeIcon>;
 export default function NavbarR() {
   return (
     <Navbar className="px-2" bg="dark" variant="dark" expand="lg">
-      <Navbar.Brand className="resp-link-nav" as={NavLink} to="home">
+      <Navbar.Brand as={NavLink} to="home">
         <FontAwesomeIcon
           className="apple-icon"
           icon={["fab", "apple"]}
@@ -26,20 +26,20 @@ export default function NavbarR() {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mx-auto">
-          <Nav.Link className="mx-3 resp-link-nav" href="#mac">
+          <Nav.Link className="mx-3" href="#mac">
             Mac
           </Nav.Link>
-          <Nav.Link className="mx-3 resp-link-nav" href="#ipad">
+          <Nav.Link className="mx-3" href="#ipad">
             Ipad
           </Nav.Link>
-          <Nav.Link className="mx-3 resp-link-nav" href="#iphone">
+          <Nav.Link className="mx-3" href="#iphone">
             Iphone
           </Nav.Link>
-          <Nav.Link className="mx-3 resp-link-nav" href="#aboutus">
+          <Nav.Link className="mx-3" href="#aboutus">
             About Us
           </Nav.Link>
           <NavDropdown
-            className="bag-icon mx-3 w-sm-25 resp-link-nav"
+            className="bag-icon mx-3 w-sm-25"
             title={title}
             id="basic-nav-dropdown"
           >
