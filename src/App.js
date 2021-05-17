4c8a9a9827d2166481c93a104f96fee8b4b2e586
Carolina <karo18delgado@gmail.com>
2021-05-16 21:26:27 -0300

    Agrego seccion About Us

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./components/FontawesomeIcons"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import NavbarR from "./components/NavbarR";
+import About from './components/About';
+
+
 
 function App() {
   return (
     <Router>
       <NavbarR />
+      <Switch>
+        <Route> 
+          <div className="fondo-pag">
+          <div className="container"> <About /></div>
+          </div>
+          </Route>
+      </Switch>
     </Router>
+
+    
   );
 }
 
