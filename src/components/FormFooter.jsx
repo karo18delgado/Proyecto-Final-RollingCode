df34577771cd69c85531a21d6e7ab1d45a3aa60e
chendo098 <rosendoalves05@gmail.com>
2021-05-17 01:17:08 -0300

    ArmadoFooter

--- src/components/FormFooter.jsx
+++ src/components/FormFooter.jsx
@@ -3,14 +3,14 @@ import Form from 'react-bootstrap/Form'
 
 export default function FormFooter() {
   return (
-    <Form className="m-2 p-2">
-  <Form.Group controlId="exampleForm.ControlInput1" className="m-2 p-2">
-    <Form.Label>Escribe tu correo</Form.Label>
-    <Form.Control type="email" placeholder="name@example.com" />
+    <Form className="">
+  <Form.Group controlId="exampleForm.ControlInput1" className="">
+    <Form.Label style={{color:'white'}}>Escribe tu correo</Form.Label>
+    <Form.Control type="email" placeholder="nombre@correo.com" />
   </Form.Group>
-  <Form.Group controlId="exampleForm.ControlTextarea1" className="m-2 p-2">
-    <Form.Label>Escribe tu consulta</Form.Label>
-    <Form.Control as="textarea" rows={3} />
+  <Form.Group controlId="exampleForm.ControlTextarea1" className="">
+    <Form.Label style={{color:'white'}}>Escribe tu consulta</Form.Label>
+    <Form.Control as="textarea" placeholder="Ingresa tu consulta" rows={3} />
   </Form.Group>
 </Form>
   );
