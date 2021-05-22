65f5039030eea54168bff8856db64e98624f3ef8
Agustin Gomez Urrutia <agustin.gu20@gmail.com>
2021-05-22 19:09:35 -0300

    modificaciones en formulario de registro y agregado de modal bloqueo

--- src/components/Admin/AdminUsuarios.jsx
+++ src/components/Admin/AdminUsuarios.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import { useState } from "react";
 import "../Admin/adminUsuarios.css";
-import { Button, Form, Table } from "react-bootstrap";
+import { Button, Form, Modal, Table } from "react-bootstrap";
 
 export default function AdminUsuarios() {
+  const [show, setShow] = useState(false);
+
+  const handleCloseBlock = () => setShow(false);
+  const handleShowBlock = () => setShow(true);
+
   return (
     <div>
       <div>
@@ -24,7 +29,8 @@ export default function AdminUsuarios() {
       <Table striped bordered hover variant="dark" className="mt-5">
         <thead>
           <tr>
-            <th>Nombre de usuario</th>
+            <th>Nombre</th>
+            <th>Apellido</th>
             <th>Email</th>
             <th>Fecha de nacimiento</th>
             <th>Sexo</th>
@@ -37,34 +43,15 @@ export default function AdminUsuarios() {
             <td>Mark</td>
             <td>Otto</td>
             <td>@mdo</td>
+            <td>01/01/1995</td>
             <td>Masculino</td>
             <td>Habilitado</td>
             <td>
-              <Button size="sm" className="btn btn-info mx-1">
-                Editar
-              </Button>
-              <Button size="sm" className="btn sm btn-success mx-1">
-                Bloqueo
-              </Button>
-              <Button size="sm" className="btn sm btn-danger mx-1">
-                Eliminar
-              </Button>
-              <Button size="sm" className="btn sm btn-warning mx-1">
-                Más información
-              </Button>
-            </td>
-          </tr>
-          <tr>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-            <td>Femenino</td>
-            <td>Habilitado</td>
-            <td>
-              <Button size="sm" className="btn btn-info mx-1">
-                Editar
-              </Button>
-              <Button size="sm" className="btn sm btn-success mx-1">
+              <Button
+                size="sm"
+                className="btn sm btn-success mx-1"
+                onClick={handleShowBlock}
+              >
                 Bloqueo
               </Button>
               <Button size="sm" className="btn sm btn-danger mx-1">
@@ -77,6 +64,33 @@ export default function AdminUsuarios() {
           </tr>
         </tbody>
       </Table>
+
+      {/* Modal editar */}
+
+      <Modal show={show} onHide={handleCloseBlock}>
+        <Modal.Header closeButton>
+          <Modal.Title>Habilitar/Deshabilitar</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="exampleForm.SelectCustom">
+              <Form.Label>Custom select</Form.Label>
+              <Form.Control as="select" custom>
+                <option>Habilitado</option>
+                <option>Deshabilitado</option>
+              </Form.Control>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseBlock}>
+            Cerrar
+          </Button>
+          <Button variant="primary" onClick={handleCloseBlock}>
+            Guardar cambios
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
