89dded3ed929bbd715af7c72e2046991fe7fd643
Carolina <karo18delgado@gmail.com>
2021-06-04 17:09:22 -0300

    Agregue mas iconos de fontawesome

--- src/components/FontawesomeIcons/index.js
+++ src/components/FontawesomeIcons/index.js
@@ -1,7 +1,8 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faShoppingBag, faUserCog } from "@fortawesome/free-solid-svg-icons";
+import { faMinus, faShoppingBag, faUserCog } from "@fortawesome/free-solid-svg-icons";
 import { faApple } from "@fortawesome/free-brands-svg-icons";
 import { faPlus, faVolumeUp } from "@fortawesome/free-solid-svg-icons";
+import { faTrashAlt } from "@fortawesome/free-regular-svg-icons";
 
 
-library.add(faApple, faShoppingBag, faUserCog, faPlus, faVolumeUp);
+library.add(faApple, faShoppingBag, faUserCog, faPlus, faVolumeUp, faTrashAlt, faMinus );
