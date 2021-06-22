28cdb5eccf085828aab2cd3e04a15d3062d40f49
Carolina <karo18delgado@gmail.com>
2021-06-21 23:08:00 -0300

    Se agrego cambios en Contador Shop

--- src/pages/ContadorShop.jsx
+++ src/pages/ContadorShop.jsx
@@ -1,46 +1,49 @@
 import { useState } from "react";
 import "../assets/shoppingcart.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { formatPrice } from "../utils/format";
+
 
 export default function ContadorShop({ cantidad, id, subtotal }) {
   const [counter, setCounter] = useState(cantidad);
-  const [precioSubtotal, setPrecioSubtotal] = useState(subtotal)
-  
- 
-  const updateCounter = (num, precioId) => {
+  const [precioSubtotal, setPrecioSubtotal] = useState(subtotal);
+
+  const updateCounter = (num) => {
     if (counter + num > 0) {
       setCounter(counter + num);
+      setPrecioSubtotal(precioSubtotal * num);
       const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
-      setPrecioSubtotal (precioId*(counter + num));
       const carritoActualizado = carrito.map((item) => {
-        if (item.id === id) return { ...item, cantidad: counter + num, subtotal: (counter+num)*precioId };
+        if (item.id === id)
+          return {
+            ...item,
+            cantidad: counter + num,
+            subtotal: precioSubtotal * num,
+          };
         else return item;
       });
-      
       localStorage.setItem("carrito", JSON.stringify(carritoActualizado));
-    
     }
   };
-  
-  
 
   return (
     <div className="row">
-      <button className="botón-shop" onClick={() => updateCounter(-1) }>
+      <button className="botón-shop" onClick={() => updateCounter(-1)}>
         <FontAwesomeIcon
           className="minus"
           icon={["fas", "minus"]}
         ></FontAwesomeIcon>
       </button>
       <h3>{counter}</h3>
-      <button className="botón-shop" onClick={() => updateCounter(1) }>
+      <button style={{ paddingRight:"40px"}} className="botón-shop" onClick={() => updateCounter(1)}>
         <FontAwesomeIcon
           className="plus"
           icon={["fas", "plus"]}
         ></FontAwesomeIcon>
       </button>
-     <h2>${precioSubtotal}</h2>
+      <h4 style={{ paddingLeft: "10px" }}>
+        {" "}
+        Total $ {counter * precioSubtotal}{" "}
+      </h4>
     </div>
   );
 }
